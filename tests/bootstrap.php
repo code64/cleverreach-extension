2a9d70ed2debd9101d751b2173a1e255c6f170cb
hofmannsven <info@hofmannsven.com>
2015-08-22 19:34:27 +0200

    Prepare unit testing with test coverage

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * CleverReach Extension PHPUnit Test Suite
+ */
+echo "\n";
+echo "\033[0;32m CleverReach Extension Test Suite \033[0m" . "\n";
+echo "\033[0;32m Version: 0.2.0 \033[0m" . "\n";
+echo "\n";
 
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
@@ -11,4 +18,3 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-
